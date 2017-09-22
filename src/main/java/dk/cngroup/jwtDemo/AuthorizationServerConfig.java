4a0251847a607037e9da7cebfc19bcd7fbb090f9
Adam Kučera <kucera@cngroup.dk>
2017-09-22 16:24:54 +0200

    Implement basic authorization server issuing JWT tokens.
     - add new dependencies on oauth and jwt
     - add AuthorizationServerConfig with JWT token settings
     - add SecurityConfig which sets up the test users

---
+++ src/main/java/dk/cngroup/jwtDemo/AuthorizationServerConfig.java
@@ -0,0 +1,64 @@
+package dk.cngroup.jwtDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private AuthenticationManager manager;
+
+
+    public AuthorizationServerConfig(AuthenticationManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()") //expose /oauth/token_key endpoint to everyone
+                .checkTokenAccess("isAuthenticated()"); //JWT tokens do not require checking via /oauth/check_token
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("jwtDemo")
+                .secret("jwtDemoSecret")
+                .scopes("read")
+                .authorizedGrantTypes("password"); //FLOWS
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore()) //where it is stored -> JWT store is not a real "store"
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(manager); //requires authentication manager
+    }
+
+    //handles JWT token encoding
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    //handles JWT signing
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("secret");
+        return converter;
+    }
+}
